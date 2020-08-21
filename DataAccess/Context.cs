d8db914790485abe5d33013590838d86907d4c9c
tomica95 <seleatoma@gmail.com>
2020-08-21 22:45:52 +0200

    architecture of project, role commands

---
+++ DataAccess/Context.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Role> Roles { get; set; }
+    }
+}
---
+++ Implementation/Commands/RoleCommands/EfCreateRoleCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Commands.Role;
+using Application;
+using Application.Dto;
+using System.Text.RegularExpressions;
+using Domain.Entities;
+using DataAccess;
+
+namespace Implementation.Commands.RoleCommands
+{
+    public class EfCreateRoleCommand : ICreateRoleCommand
+    {
+        private readonly Context _context;
+        public int Id => 1;
+
+        public string Name => "Create new Role";
+
+        public EfCreateRoleCommand(Context context) 
+        {
+            _context = context;
+        }
+
+        public void Execute(RoleDTO dto)
+        {
+            var role = new Role
+            {
+                Name = dto.Name
+            };
+
+            _context.Roles.Add(role);
+            _context.SaveChanges();
+        }
+    }
+}
